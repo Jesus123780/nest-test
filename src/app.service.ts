9c7c5a34a0b5e0ac2f754918eb03d9a74727d336
Cristian <cristianjuvinao801@gmai.com>
2024-01-09 09:43:48 -0500

    feat: update test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Jesus Juvinao Backend Nestjs +57 3014548087';
   }
 }
