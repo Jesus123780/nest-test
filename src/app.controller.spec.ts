004e107088ce0878d88d306623922e655d3d9eac
Cristian <cristianjuvinao801@gmai.com>
2024-01-09 10:02:18 -0500

    feat: update version and fix test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Jesus Juvinao Backend Nestjs +57 3014548087!"', () => {
+      expect(appController.getHello()).toBe('Jesus Juvinao Backend Nestjs +57 3014548087');
     });
   });
 });
